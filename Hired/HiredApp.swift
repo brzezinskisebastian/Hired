64c981644f9a6515efbaa4b51376c99bc1b33d5f
Sebastian Brzeziński <brzezinski.sebastian@icloud.com>
2025-03-22 12:19:40 +0100

    Initial Commit

---
+++ Hired/HiredApp.swift
@@ -0,0 +1,17 @@
+//
+//  HiredApp.swift
+//  Hired
+//
+//  Created by Sebastian Brzeziński on 22/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct HiredApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
